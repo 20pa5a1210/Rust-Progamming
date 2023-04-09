7503710e98940d2b7a45beea3bd99c7857c9186c
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-04-09 17:17:48 +0530

    Struct and Impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
 fn main() {
     println!("Hello, world!");
-    Struct hello{
-        
+    enum Hello {
+        description,
+        rel,
+        is_ready,
     }
+    reverse("Hello, world!");
+    struct City {
+        name: String,
+        population: u32,
+        is_capital: bool,
+    }
+
+    impl City {
+        fn new(name: &str, population: u32, is_capital: bool) -> City {
+            City {
+                name: name.to_string(),
+                population,
+                is_capital,
+            }
+        }
+    }
+
+    let london = City::new("London", 8_615_000, true);
 }
+
+fn reverse(input: &str) -> String {
+    let mut chars: Vec<char> = input.chars().collect();
+    chars.reverse();
+    chars.into_iter().collect()
+}
+
+// enum City {
+//     London,
+//     Paris,
+//     Berlin,
+//     NewYork,
+// }
