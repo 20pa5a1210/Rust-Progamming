6c525654689cd706241fbc1272ead812eaa4ab63
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-04-09 19:32:10 +0530

    Delloc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let total_orders: i64 = get_final_orders();
+    println!("Total orders: {}", total_orders);
+}
+
+fn get_final_orders() -> i64 {
+    let orders: Vec<i64> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let mut total_orders: i64 = 0;
+    for order in orders.iter() {
+        total_orders += order;
+    }
+    return total_orders;
 }
