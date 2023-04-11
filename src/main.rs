a49dddd00bab197853b52d226682f02d83ad0f03
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-04-11 15:20:40 +0530

    Slice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+    println!("{}", word);
+    s.clear();
+    let s = String::from("Hello world");
+    let hello = &s[..];
+    println!("{}", hello);
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+    println!("{:?}", slice);
+    assert_eq!(slice, &[2, 3]);
+    println!("{:?}", assert_eq!(slice, &[2, 3]));
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
