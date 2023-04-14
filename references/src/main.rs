6d317f76391a14605f3efb5e410141fa02d0ac71
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-04-14 22:07:52 +0530

    Custom Tests

--- references/src/main.rs
+++ references/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     println!("Hello, world!");
     let sum = array_sum(&[1, 2, 3, 4, 5]);
     println!("sum = {}", sum);
+    let sum1 = array_sum(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    println!("sum1 = {}", sum1);
+    let sum2 = add_sum(1, 2);
+    println!("sum2 = {}", sum2);
 }
 fn array_sum(array: &[i32]) -> i32 {
     let mut sum = 0;
@@ -9,4 +13,25 @@ fn array_sum(array: &[i32]) -> i32 {
         sum += i;
     }
     sum
+}
+
+fn add_sum(num1: i32, num2: i32) -> i32 {
+    num1 + num2
+}
+
+// Path: references/src/lib.rs
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_array_sum() {
+        let sum = array_sum(&[1, 2, 3, 4, 5]);
+        println!("sum1 = {}", sum);
+        assert_eq!(sum, 15);
+        let sum1 = array_sum(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        println!("sum1 = {}", sum1);
+        assert_eq!(sum1, 55);
+        assert_eq!(add_sum(1, 2), 3);
+    }
 }
---
+++ references/target/debug/deps/references-79e9cec02cb4b96c.d
@@ -0,0 +1,5 @@
+/home/suryanarayana/Documents/Projects Github Commits/Rust-Progamming/references/target/debug/deps/references-79e9cec02cb4b96c: src/main.rs
+
+/home/suryanarayana/Documents/Projects Github Commits/Rust-Progamming/references/target/debug/deps/references-79e9cec02cb4b96c.d: src/main.rs
+
+src/main.rs:
