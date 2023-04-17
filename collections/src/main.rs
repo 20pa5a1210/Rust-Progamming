e87b013d54c4377a36e1a8103d5a7f62cc495507
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-04-17 21:47:37 +0530

    iter Vectors

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -5,17 +5,23 @@ fn main() {
     v.push(5);
     v.push(6);
     v.push(7);
-    v.push(8);  
+    v.push(8);
 
     let third: &i32 = &v[2];
     println!("The third element is {}", third);
-    let third : Option<&i32> = v.get(2);
+    let third: Option<&i32> = v.get(2);
     match third {
         Some(third) => println!("The third element is {}", third),
         None => println!("There is no third element."),
     }
 
+    let first = &v[0];
+    // v.push(6);
+    println!("The first element is: {}", first);
 
+    let mut v = vec![100, 32, 57, 89, 12, 45, 67, 90,];
 
-
+    for i in &mut v {
+        println!("{}", i);
+    }    
 }
